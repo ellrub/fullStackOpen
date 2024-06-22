62661dfabaf04b3dd8ec46b17643fa9e2ccfe8b8
Ruben Ellefsen <ellefsen88@gmail.com>
2024-06-23 01:05:27 +0200

    part1.1 setup

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,46 @@
+const Header = (props) => {
+  console.log(props)
+  return (
+    <>
+      <h1>{props.course}</h1>
+    </>
+  )
+}
+
+const Content = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <Part />
+      
+    </div>
+  )
+}
+
+// const Total = (props) => {
+//   console.log(props)
+//   return (
+//     <>
+//     </>
+//   )
+// }
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content />
+      {/* <Total /> */}
+    </div>
+  )
+}
+
+export default App
